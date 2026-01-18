46039b175697b1a42b8ba8812f343c94a874d008
Dmytrii Vitman <dimawittmann@gmail.com>
2026-01-18 17:45:34 +0100

    v0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 1
-        versionName = "0.3.3"
+        versionName = "0.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
